253b0d3c81adaca0e1b3424842f0820de09124f7
Samuel Holden <samuelchanceholden@gmail.com>
2025-05-07 18:52:28 -0700

    made the item boxes respawn after 5 seconds!

--- itemBox/item_box.gd
+++ itemBox/item_box.gd
@@ -3,16 +3,24 @@ var itemBoxExists = true
 var rot_x = deg_to_rad(50)
 var rot_y = deg_to_rad(30)
 
+var waitTime = -1
 func _process(delta: float) -> void:
-	
+	waitTime -= delta
 	if itemBoxExists:
+		$BoxMesh/Area3D.monitoring = true
+		$BoxMesh.visible = true
 		$BoxMesh.rotate_x(rot_x * delta)
 		$BoxMesh.rotate_y(rot_y * delta)
+	else:
+		if waitTime <= 0:
+			itemBoxExists = true
+		$BoxMesh/Area3D.monitoring = false
+		$BoxMesh.visible = false
 
 
 func _on_area_3d_area_entered(area: Area3D) -> void:
 	if area.get_parent().identification == "kart":
 		area.get_parent().powerup()
-		$BoxMesh.queue_free()
 		itemBoxExists = false
+		waitTime = 5
 		$Pickup.play()
